27266dc2ce58c1c2e619e0dc1c881dc5b9882089
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2022-12-13 11:10:21 -0300

    Add routes, created first screen

---
+++ tga/src/routes/stackRoutes.tsx
@@ -0,0 +1,21 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { VotesScreen } from '../screens/VotesScreen'
+import { WinnerScreen } from '../screens/WinnerScreen'
+
+const {Screen, Navigator} = createBottomTabNavigator()
+
+export function StackRoutes(){
+    return(
+        <Navigator>
+            <Screen
+                name='Votes'
+                component={VotesScreen}
+            />
+
+            <Screen
+                name='Winner'
+                component={WinnerScreen}
+            />
+        </Navigator>
+    )
+}

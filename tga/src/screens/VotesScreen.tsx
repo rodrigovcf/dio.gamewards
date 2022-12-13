aee922d3fe8e14eb7451b1b9599847765eabd0ad
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2022-12-13 12:16:13 -0300

    Adjustes in the VotesScreen and test View

--- tga/src/screens/VotesScreen.tsx
+++ tga/src/screens/VotesScreen.tsx
@@ -1,19 +1,46 @@
-import React from "react"
-import {View, Text, StyleSheet} from "react-native"
+import React, {useEffect, useState} from "react"
+import {View, Text, StyleSheet, ScrollView} from "react-native"
+
+import {GameCard} from '../components/Votes/GameCard'
+import {Header} from '../components/Votes/Header'
+
+import {clientGetGames} from '../api/api'
 
 export function VotesScreen(){
+
+    const [gameList, setGameList] = useState([{}])
+
+    useEffect(() => {
+
+        (async () => {
+            const response = await clientGetGames()
+            setGameList(response)
+        })()
+
+    }, [])
+
     return(
         <View style={styles.container}>
-            <Text>Exodia Obliterar</Text>
+            <Header />
+            <ScrollView>
+                {gameList.map(game => GameCard(game))}
+            </ScrollView>            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor: '#191919',
-        alignItems: 'center',
-        justifyContent: 'center'
+    container: {
+      flex: 1,
+      color:'#fff',
+      backgroundColor: '#191919',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    gameArea:{
+      flex:1,
+      paddingTop:10,
+      paddingBottom: 20,
+      width:'100%'
     }
-})
+  });

4c0e80c58fcd4597bfd611d16898d0a11a5a9fcc
Rodrigo Valença <rodrigovalenca@MacBook-Pro-de-Rodrigo.local>
2022-12-13 11:53:35 -0300

    Config api and impl GameCard

---
+++ tga/src/api/api.ts
@@ -0,0 +1,28 @@
+const baseApiURL = "http://192.168.0.15:8080/api/"
+
+export async function clientGetGames(){
+    //Faço a requisição com o endpoint da api
+    const response = await fetch(`${baseApiURL}games`)
+
+    //Agora extraio o JSON que recebi da api
+    const json = await response.json()
+    return json
+}
+
+export function clientSendingVotes(id: number){
+    
+    const requestOption = {
+        method:'PATCH'
+    }
+
+    fetch(`${baseApiURL}games/${id}/vote`, requestOption)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch( error => console.log('error', error))        
+}
+
+export async function clientGetWinner(){
+    const response = await fetch(`${baseApiURL}games`)
+    const json = await response.json()
+    return json[0]
+}
